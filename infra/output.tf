52c7ec13c8d9bb1966ef2e8c4ad4753352a9ba7c
itonix <tonyshery@gmail.com>
2025-11-13 04:02:03 +0530

    removed ec2 server ip output

--- infra/output.tf
+++ infra/output.tf
@@ -10,10 +10,7 @@ output "s3_bucket_name" {
   value = aws_s3_bucket.blog_app_bucket.bucket
 }
 
-output "ec2-instanceIp" {
-  value = aws_instance.app_server.public_ip
 
-}
 output "asg_name" {
   value = module.autoscaling.autoscaling_group_name
 }
